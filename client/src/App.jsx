741866dafecc294578f566d34bb02bf4586ca329
shrutiverma2208 <shrutiverma2208@gmail.com>
2023-11-04 10:49:37 +0530

    complete create listing page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter,Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import PrivateRoute from "./components/PrivateRoute"
 import About from "./pages/About"
+import CreateListing from "./pages/CreateListing"
 import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
@@ -17,7 +18,8 @@ const App = () => {
       <Route path='/sign-up' element={<SignUp/>} />
       <Route path='/about' element={<About/>} />
       <Route element={<PrivateRoute/>}>
-      <Route path='/profile' element={<Profile/>} />
+       <Route path='/profile' element={<Profile/>} />
+       <Route path='/create-listing' element={<CreateListing/>} />
       </Route>
     </Routes>
     </BrowserRouter>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/st
 import { app } from "../firebase";
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import {Link} from 'react-router-dom';
 
 export default function Profile  () {
   const fileRef =useRef(null)
@@ -170,6 +171,10 @@ const handleSignOut =async()=>{
           p-3 uppercase hover:opacity-95 disabled:opacity-60">
             {loading ? 'Loading...' : 'Update'}
           </button>
+          <Link className="bg-green-700 text-white p-3 rounded-lg
+          uppercase text-center hover:opacity-95" to={"/create-listing"}>
+            Create Listing
+          </Link>
         </form>
         <div className="flex justify-between mt-5"> 
           <span onClick={handleDeleteUser} 
