e65a8ed510efe992a7f1687c3494ce60867b98db
shrutiverma2208 <shrutiverma2208@gmail.com>
2023-11-08 14:20:51 +0530

    complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,8 @@ import { app } from "../firebase";
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import {Link} from 'react-router-dom';
+import { async } from "@firebase/util";
+import Listing from "../../../api/models/listing.model";
 
 export default function Profile  () {
   const fileRef =useRef(null)
@@ -14,6 +16,8 @@ export default function Profile  () {
   const [fileUploadError, setFileUploadError]=useState(false);
   const [formData, setFormData]=useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingError, setShowListingError]= useState(false)
+  const [userListings, setUserListings]=useState([])
  const dispatch =useDispatch();
   //firebase storage
   //allow read;
@@ -108,6 +112,21 @@ const handleSignOut =async()=>{
   }
 }
 
+const handleShowListings = async() =>{
+  try {
+    setShowListingError(false);
+    const res = await fetch(`/api/user/listings/${currentUser._id}`);
+    const data= await res.json();
+    if(data.success === false){
+      setShowListingError(true);
+      return;
+    }
+    setUserListings(data);
+  } catch (error) {
+    setShowListingError(true)
+  }
+}
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>
@@ -185,6 +204,41 @@ const handleSignOut =async()=>{
         <p className="text-red-700 mt-5">{error ? error : ''}</p>
         <p className="text-green-700 mt-5">
           {updateSuccess ?'User is updated successfully!':''}</p>
+          <button onClick={handleShowListings}
+          className='text-green-700 w-full'>Show Listings</button>
+          <p className="text-red-700 mt-5">{showListingError ?
+          'Error showing listings':''}</p>
+
+           {userListings &&
+           userListings.length > 0 &&
+           <div className="flex flex-col gap-4">
+            <h1 className="text-center mt-7 text-2xl
+            font-semibold">Your Listings</h1>
+           {userListings.map((listing) =>(
+            <div key={listing._id} 
+            className='border rounded-lg
+            p-3 flex justify-between items-center gap-4'>
+              <Link to={`/listing/${listing._id}`}>
+                <img 
+                src={listing.imageUrls[0]} 
+                alt='listing cover'
+                className="h-16 w-16 object-contain"/>
+              </Link>
+              <Link className="text-slate-700 font-semibold
+                 hover:underline truncate" to={`/listing/${listing._id}`}>
+                <p >{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col">
+                <button className="text-red-700 
+                uppercase">Delete</button>
+                <button className="text-green-700 
+                uppercase">Edit</button>
+
+              </div>
+              </div>
+           ))}
+          </div>}
           </div>
   );
   
